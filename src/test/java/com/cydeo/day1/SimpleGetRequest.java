92126878e4141535c9aa90dcaa591546b8930691
Adem OZTURK <admoztrk86@gmail.com>
2023-03-28 17:10:18 +0200

    new class is created and new changes are added

--- src/test/java/com/cydeo/day1/SimpleGetRequest.java
+++ src/test/java/com/cydeo/day1/SimpleGetRequest.java
@@ -9,12 +9,15 @@ public class SimpleGetRequest {
     String url = "http://3.89.232.139:8000/api/spartans";
 
     @Test
-    public void test1(){
+    public void test1() {
 
+        // send a get request ans save response inside the response object
         Response response = RestAssured.get(url);
 
+        // print response status code
         System.out.println(response.statusCode());
 
+        // print response body
         response.prettyPrint();
 
 
