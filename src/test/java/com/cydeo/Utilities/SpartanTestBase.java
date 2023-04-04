cadddddbf53b4c0ffecaecb01d8c89742da08873
Adem OZTURK <admoztrk86@gmail.com>
2023-04-04 18:16:01 +0200

    test base class updated

--- src/test/java/com/cydeo/Utilities/SpartanTestBase.java
+++ src/test/java/com/cydeo/Utilities/SpartanTestBase.java
@@ -1,6 +1,6 @@
 package com.cydeo.Utilities;
 
-import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.*;
@@ -9,7 +9,21 @@ public class SpartanTestBase {
 
     @BeforeAll
     public static void init(){
+
+        //save baseurl inside this variable so that we dont need to type each http method.
         baseURI= "http://3.87.228.6:8000";
 
+        String dbUrl = "jdbc:oracle:thin:@3.87.228.6:1521:XE";
+        String dbUsername = "SP";
+        String dbPassword = "SP";
+
+        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+    }
+
+
+    @AfterAll
+    public static void teardown(){
+
+        DBUtils.destroy();
     }
 }
